86dff48d5206b76521a42e9b6bca4132da9fa391
Emediong-lang <61968349+Emediong-lang@users.noreply.github.com>
2020-03-26 16:23:10 +0100

    Add files via upload

---
+++ Javascripttask1.js
@@ -0,0 +1,10 @@
+let name = "emediong-lang";
+console.log("My name is: " + name);
+
+var courses
+courses = ["HTML", "CSS", "JavaScript"]
+console.log("My courses are: " + courses)
+
+for (i = 0; i <= 200; i++){
+  if(i%2 !== 0) console.log(i);
+}
